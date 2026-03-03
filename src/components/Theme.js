1f87ce70bccc87c991142982a24d252fdd325267
Ankit Kumar <ankitkr5447@gmail.com>
2026-03-03 15:20:00 +0530

    fixed bug

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,6 +1,7 @@
 function updateStatusBar(isDark) {
+  console.log(("Theme Change Called"))
 	const meta = document.getElementById("statusBar");
-	if (meta) meta.setAttribute("content", isDark ? "#0f172b" : "#ffffff");
+	if (meta) meta.setAttribute("content", isDark ? "#000000" : "#ffffff");
 }
 
 export { updateStatusBar };
