17fb35d5fe4eb82c227d0fd1b1a82b950b40cccd
Ankit Kumar <ankitkr5447@gmail.com>
2026-04-02 18:44:53 +0530

    ui when user is not logged in

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.svoio962jm8"
+    "revision": "0.gqoha32ftfg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
